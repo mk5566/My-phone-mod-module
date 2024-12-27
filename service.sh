2194cceb1b1b75723f7e2d5ab8702485594eb43e
Mika Liao <mika_liao@alatech.com.tw>
2024-12-27 10:03:31 +0800

    update miscs

--- service.sh
+++ service.sh
@@ -6,3 +6,8 @@ resetprop -n debug.renderengine.backend skiavkthreaded
 sleep 20
 su -c "echo 0 > /sys/block/zram0/disksize"
 su -c "swapoff /dev/block/zram0"
+su -c "logcat -c"
+su -c "stop logd"
+su -c "setprop init.svc.logd stopped"
+su -c "setprop persist.logd.enable 0"
+su -c "setprop persist.logd.size 0"
--- system/bin/logcatlog.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-# This is a dummy file to disable the original init script
-exit 0
